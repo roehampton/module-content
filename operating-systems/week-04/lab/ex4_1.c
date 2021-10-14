f60eb1c8ad17e94d2af73bdb9bff10b1b427038c
Kevin Chalmers <drkevinchalmers@gmail.com>
2021-10-14 09:03:04 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ operating-systems/week-04/lab/ex4_1.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+// Thread function
+void* do_work(void *arg)
+{
+    printf("I'm a thread!\n");
+    // Exit the thread
+    pthread_exit(NULL);
+}
+
+int main(int argc, char **argv)
+{
+    pthread_t thread;
+    // Create a thread
+    pthread_create(&thread, NULL, do_work, NULL);
+    printf("I'm the main program!\n");
+    void *status;
+    // Join the thread
+    pthread_join(thread, &status);
+    return 0;
+}
